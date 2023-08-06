e04420fd96c69a14afc6b012ab42283e02d35cde
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-06 08:35:18 +0200

    refactor: navbar to components

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -4,19 +4,20 @@ import Apple from '../../assets/icons/apple.svg';
 import ControlCenter from '../../assets/icons/controlCenter.svg';
 import Wifi from '../../assets/icons/wifi.svg';
 import Notch from '../../assets/ui/notch.svg';
+import Image from 'next/image';
 export default function Navbar() {
   return (
     <div className="flex justify-between bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 pb-2">
       <div className="flex items-center">
-        <Apple />
+        <Image src={Apple} alt="apple logo" />
       </div>
       <div>
-        <Notch />
+        <Image src={Notch} alt="apple logo" />
       </div>
       <div className="flex items-center">
         <Battery />
-        <Wifi />
-        <ControlCenter />
+        <Image src={Wifi} alt="apple logo" />
+        <Image src={ControlCenter} alt="apple logo" />
         <DateNTime />
       </div>
     </div>
