bc9aadef7a233184354d5d005f3bea71b647f43f
elmersson <elmerssonrasmus@gmail.com>
2023-08-07 16:50:13 +0200

    Ohd 40 (#11)
    * feat: desktop init
    
    * feat: build dock and items
    
    ---------
    
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Wallpapers from '@/components/atoms/Wallpapers';
+import Desktop from '@/components/Pages/Desktop';
 import Login from '@/components/Pages/Login';
 
 export default function Home() {
   return (
     <main>
       <Wallpapers />
-      <Login />
+      <Desktop />
     </main>
   );
 }
---
+++ src/components/Pages/Desktop.tsx
@@ -0,0 +1,11 @@
+import Dock from '../organisms/Dock';
+import Navbar from '../organisms/Navbar';
+
+export default function Desktop() {
+  return (
+    <div>
+      <Navbar />
+      <Dock />
+    </div>
+  );
+}
