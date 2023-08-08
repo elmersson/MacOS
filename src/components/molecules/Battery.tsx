9fc8fb835580c6a1920fa073da49bfed5599eef1
elmersson <elmerssonrasmus@gmail.com>
2023-08-08 18:48:25 +0200

    feat: charging indicator added and refactor (#12)
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

--- src/components/molecules/Battery.tsx
+++ src/components/molecules/Battery.tsx
@@ -1,71 +1,29 @@
 'use client';
 
+import Image from 'next/image';
 import { useBattery } from '../../hooks/useBattery';
-
-function mapBatteryStateToRange(batteryState: number) {
-  if (batteryState < 0 || batteryState > 1) {
-    throw new Error('Battery state must be between 0 and 1');
-  }
-
-  const newMin = 0;
-  const newMax = 19;
-
-  // Scale the battery state to the new range
-  const scaledValue = batteryState * (newMax - newMin) + newMin;
-
-  return scaledValue;
-}
+import Charging from '@/assets/icons/charging.svg';
+import BatteryLevelIndicator from '../atoms/BatteryLevelIndicator';
 
 export default function Battery() {
   const batteryState = useBattery();
 
-  const batteryWidth = mapBatteryStateToRange(batteryState.level).toFixed(0);
-
-  const color = (): string => {
-    if (batteryState.level < 0.11) return 'rgba(255, 78, 78, 0.92)';
-    else return 'rgba(255, 255, 255, 0.92)';
-  };
-
   return (
-    <div className="flex mx-2 items-center">
+    <div className="flex items-center">
       <p className="text-white text-xs text-shadow mx-1">
         {(batteryState.level * 100).toFixed(0)} %
       </p>
-      <div className="drop-shadow">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="26"
-          height="13"
-          viewBox="0 0 26 13"
-          fill="none"
-        >
-          <g clipPath="url(#clip0_208_1555)">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M0 3.5C0 1.84315 1.34315 0.5 3 0.5H20C21.6569 0.5 23 1.84315 23 3.5V9.5C23 11.1569 21.6569 12.5 20 12.5H3C1.34315 12.5 0 11.1569 0 9.5V3.5ZM1 3.5C1 2.39543 1.89543 1.5 3 1.5H20C21.1046 1.5 22 2.39543 22 3.5V9.5C22 10.6046 21.1046 11.5 20 11.5H3C1.89543 11.5 1 10.6046 1 9.5V3.5ZM25.5 6.5C25.5 7.61042 24.8967 8.57994 24 9.09865V3.90135C24.8967 4.42006 25.5 5.38958 25.5 6.5Z"
-              fill="rgba(255, 255, 255, 0.92)"
-            />
-            <rect
-              x="2"
-              y="2.5"
-              width={batteryWidth}
-              height="8"
-              rx="1"
-              fill={color()}
-            />
-          </g>
-          <defs>
-            <clipPath id="clip0_208_1555">
-              <rect
-                width="25.5"
-                height="12"
-                fill="rgba(255, 255, 255, 0.92)"
-                transform="translate(0 0.5)"
-              />
-            </clipPath>
-          </defs>
-        </svg>
+      <div className="relative flex items-center">
+        <BatteryLevelIndicator batteryLevel={batteryState.level} />
+        {batteryState.charging && (
+          <Image
+            src={Charging}
+            alt="charging"
+            height={9.8}
+            width={9.8}
+            className="absolute top-1/2 -mt-2 left-2.5 ml-1"
+          />
+        )}
       </div>
     </div>
   );
--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -5,21 +5,22 @@ import ControlCenter from '../../assets/icons/controlCenter.svg';
 import Wifi from '../../assets/icons/wifi.svg';
 import Notch from '../../assets/ui/notch.svg';
 import Image from 'next/image';
+
 export default function Navbar() {
   return (
-    <div className="flex justify-between bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 pb-2 px-2 items-center">
+    <div className="flex justify-between bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 pb-2 px-2 relative">
       <div className="flex items-center">
         <Image src={Apple} alt="apple logo" className="drop-shadow mx-2" />
       </div>
-      <div>
+      <div className="flex justify-center absolute inset-0">
         <Image src={Notch} alt="notch" />
       </div>
       <div className="flex items-center">
         <Battery />
-        <Image src={Wifi} alt="apple logo" className="drop-shadow mx-2" />
+        <Image src={Wifi} alt="wifi logo" className="drop-shadow mx-2" />
         <Image
           src={ControlCenter}
-          alt="apple logo"
+          alt="control center logo"
           className="drop-shadow mx-2"
         />
         <DateNTime />
