87ad17f27818e6d3c9569e2e8d574e8607daff3c
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-16 19:19:11 +0200

    feat: boot

---
+++ src/components/Pages/Boot.tsx
@@ -0,0 +1,30 @@
+'use client';
+import Image from 'next/image';
+import Apple from '@/assets/icons/apple.svg';
+import { useState, useEffect } from 'react';
+
+export default function Boot() {
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    if (progress < 100) {
+      const interval = setInterval(() => {
+        setProgress((prevProgress) => (prevProgress + 1) % 101);
+      }, 50);
+
+      return () => clearInterval(interval);
+    }
+  }, [progress]);
+
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center items-center bg-black">
+      <Image src={Apple} alt="Apple logo" width={80} />
+      <div className="h-1 w-56 rounded-full bg-gray-500 mt-4">
+        <div
+          className="h-1 rounded-full bg-white"
+          style={{ width: `${progress}%`, transition: 'width 0.5s ease' }}
+        ></div>
+      </div>
+    </div>
+  );
+}
