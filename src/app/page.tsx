9668beac57fec60f31f0cf2e980950c25e6cf34b
elmersson <elmerssonrasmus@gmail.com>
2023-08-16 19:20:53 +0200

    feat: boot (#15)
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import Wallpapers from '@/components/atoms/Wallpapers';
+import Boot from '@/components/Pages/Boot';
 import Desktop from '@/components/Pages/Desktop';
 import Login from '@/components/Pages/Login';
 
 export default function Home() {
-  return (
-    <>
-      <Wallpapers />
-      <Desktop />
-    </>
-  );
+  return <Boot />;
 }
