36bb21e137c26998d3a05ec101cfcada6b84e13f
elmersson <elmerssonrasmus@gmail.com>
2023-08-17 16:47:26 +0200

    Ohd 50 boot (#16)
    * feat: boot
    
    * feat: zustand and first store
    
    ---------
    
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

---
+++ src/lib/store.ts
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+import { ComputerStatus, computerStatusSlice } from "./slices/computerStatusSlice";
+
+export const useStore = create<ComputerStatus>((...a) => ({
+  ...computerStatusSlice(...a),
+}));
