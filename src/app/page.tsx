60ef9b100295b2a9cfed281529fad72fa03bc70f
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-18 15:04:46 +0200

    feat: zustand and user flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import Wallpapers from '@/components/atoms/Wallpapers';
+'use client';
 import Boot from '@/components/Pages/Boot';
 import Desktop from '@/components/Pages/Desktop';
 import Login from '@/components/Pages/Login';
+import Wallpapers from '@/components/atoms/Wallpapers';
+import { useStore } from '@/lib/store';
 
 export default function Home() {
-  return <Boot />;
+  const { booted, logedIn } = useStore();
+
+  if (!booted) {
+    return <Boot />;
+  } else if (!logedIn) {
+    return (
+      <>
+        <Login />
+        <Wallpapers />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Desktop />
+        <Wallpapers />
+      </>
+    );
+  }
 }
