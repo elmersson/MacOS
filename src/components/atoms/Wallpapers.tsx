a71f8632b13b1eea7e555cd9924ee86ca1b5d57b
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-20 10:41:46 +0200

    fix: darkmode for the wallpaper

--- src/components/atoms/Wallpapers.tsx
+++ src/components/atoms/Wallpapers.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image';
 import WallpaperDay from '../../assets/images/wallpapers/wallpaper-day.jpg';
+import WallpaperNight from '../../assets/images/wallpapers/wallpaper-night.jpg';
+import { useTheme } from 'next-themes';
 
 export default function Wallpapers() {
+  const { theme } = useTheme();
   return (
     <div className="absolute inset-0 -z-10">
-      <Image src={WallpaperDay} alt="background image" fill />
+      <Image
+        src={theme === 'light' ? WallpaperDay : WallpaperNight}
+        alt="background image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
     </div>
   );
 }
