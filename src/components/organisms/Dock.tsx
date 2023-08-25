7e3adfd37d5d7d7bbebd361a6e5193e1968f392b
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-25 16:54:51 +0200

    feat: hide and show dock

--- src/components/organisms/Dock.tsx
+++ src/components/organisms/Dock.tsx
@@ -1,67 +1,41 @@
-'use client';
-import Image from 'next/image';
-import Github from '@/assets/apps/github.png';
-import Finder from '@/assets/apps/finder.png';
-import Mail from '@/assets/apps/MAIL.png';
-import LaunchPad from '@/assets/apps/launchpad.png';
-import VsCode from '@/assets/apps/vscode.png';
-import Safari from '@/assets/apps/safari.png';
-import Iterm2 from '@/assets/apps/iterm2.png';
-import Notes from '@/assets/apps/notes.png';
+import React, { useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import apps from '@/data/Apps';
+import DockItem from '../molecules/DockItem';
 
 export default function Dock() {
+  const [isDockFocused, setIsDockFocused] = useState(false);
+  const controls = useAnimation();
+
+  const initialY = '120%';
+  const animateY = isDockFocused ? '0%' : '120%';
+
+  const handleMouseEnter = () => {
+    controls.start({ y: '0%' });
+    setIsDockFocused(true);
+  };
+
+  const handleMouseLeave = () => {
+    controls.start({ y: initialY });
+    setIsDockFocused(false);
+  };
+
   return (
-    <div className="fixed bottom-2 left-0 w-full flex justify-center">
-      <ul className="bg-clip-padding backdrop-filter backdrop-blur-md bg-neutral-300/10 border border-neutral-100/20 flex rounded-3xl p-1">
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Finder} alt="github" className="w-14" />
-            <div className="h-1 w-1 m-0 rounded-full bg-slate-950/80 dark:bg-slate-50/80 mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={LaunchPad} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Safari} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Mail} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Iterm2} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={VsCode} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Github} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full mt-1"></div>
-          </div>
-        </li>
-        <li className="flex justify-center">
-          <div className="flex flex-col items-center">
-            <Image src={Notes} alt="github" className="w-14" />
-            <div className="h-1 w-1 rounded-full bg-slate-50 dark:bg-slate-900 mt-1"></div>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <motion.div
+      className="fixed bottom-2 left-0 w-full flex justify-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <motion.ul
+        className="bg-clip-padding backdrop-filter backdrop-blur-md bg-neutral-300/10 border border-neutral-100/20 flex rounded-3xl p-1"
+        initial={{ y: initialY }}
+        animate={{ y: animateY }}
+        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+      >
+        {apps.map((app) => (
+          <DockItem id={app.id} title={app.title} img={app.img} key={app.id} />
+        ))}
+      </motion.ul>
+    </motion.div>
   );
 }
