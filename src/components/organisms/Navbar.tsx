22d05f1c3c06d9fdc19069a43261c572e582bfc4
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-08-27 14:28:39 +0200

    style: changed notch to lay on top

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -10,31 +10,44 @@ import { useStore } from '@/lib/store';
 export default function Navbar() {
   const { logedIn } = useStore();
   return (
-    <div
-      className={`flex justify-between ${
-        logedIn
-          ? 'bg-slate-800 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 dark:bg-slate-800/40'
-          : ''
-      } pb-2 px-2 relative`}
-    >
-      <div className="flex items-center">
-        {logedIn && (
-          <Image src={Apple} alt="apple logo" className="drop-shadow mx-2" />
-        )}
+    <>
+      <div className="fixed inset-x-0 top-0 flex justify-center z-50">
+        <Image src={Notch} alt="notch" height={40} />
       </div>
-      <div className="flex justify-center absolute inset-0">
-        <Image src={Notch} alt="notch" />
+      <div
+        className={`flex justify-between ${
+          logedIn
+            ? 'bg-slate-800 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 dark:bg-slate-800/40'
+            : ''
+        } py-3 px-2 relative`}
+      >
+        <div className="flex items-center">
+          {logedIn && (
+            <Image
+              src={Apple}
+              alt="apple logo"
+              height={25}
+              className="drop-shadow mx-2"
+            />
+          )}
+        </div>
+        <div className="flex items-center space-x-5">
+          <Battery />
+          <Image
+            src={Wifi}
+            height={13}
+            alt="wifi logo"
+            className="drop-shadow"
+          />
+          <Image
+            src={ControlCenter}
+            height={13}
+            alt="control center logo"
+            className="drop-shadow"
+          />
+          <DateNTime />
+        </div>
       </div>
-      <div className="flex items-center space-x-5">
-        <Battery />
-        <Image src={Wifi} alt="wifi logo" className="drop-shadow" />
-        <Image
-          src={ControlCenter}
-          alt="control center logo"
-          className="drop-shadow"
-        />
-        <DateNTime />
-      </div>
-    </div>
+    </>
   );
 }
