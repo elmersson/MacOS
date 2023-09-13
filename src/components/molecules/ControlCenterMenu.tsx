020ef6d8b301d771c87f7e0eda5ef3def93b52dd
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-13 19:30:19 +0200

    feat: connnectcontrol created

--- src/components/molecules/ControlCenterMenu.tsx
+++ src/components/molecules/ControlCenterMenu.tsx
@@ -1,14 +1,8 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import { MdWifi, MdWifiTethering } from 'react-icons/md';
-import {
-  IoBluetooth,
-  IoMan,
-  IoPauseSharp,
-  IoPlayForward,
-  IoApps,
-} from 'react-icons/io5';
+import { IoMan, IoPauseSharp, IoPlayForward, IoApps } from 'react-icons/io5';
 import Image from 'next/image';
 import Stockholmsvy from '../../assets/images/stockholmsvy.jpeg';
+import ConnectControl from './ConnectControl';
 
 interface ControlCenterMenuProps {
   isVisible: boolean;
@@ -43,38 +37,10 @@ export default function ControlCenterMenu({
         >
           <div className="rounded-xl px-2 py-2 w-[300px] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/30 space-y-2">
             <div className="flex flex-row space-x-2">
-              <div className="rounded-md px-3 py-1 w-[100%] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/10 shadow-md flex flex-col justify-evenly">
-                <div className="flex flex-row space-x-2">
-                  <div className="h-7 w-7 bg-blue-600 rounded-full flex justify-center items-center">
-                    <MdWifi style={{ color: 'white' }} />
-                  </div>
-                  <div>
-                    <p className="text-xs font-semibold">Wi-Fi</p>
-                    <p className="text-xxs">NETGEAR15-5G</p>
-                  </div>
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <div className="h-7 w-7 bg-blue-600 rounded-full flex justify-center items-center">
-                    <IoBluetooth style={{ color: 'white' }} />
-                  </div>
-                  <div>
-                    <p className="text-xs font-semibold">Bluetooth</p>
-                    <p className="text-xxs">On</p>
-                  </div>
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <div className="h-7 w-7 bg-blue-600 rounded-full flex justify-center items-center">
-                    <MdWifiTethering style={{ color: 'white' }} />
-                  </div>
-                  <div>
-                    <p className="text-xs font-semibold">Airdrop</p>
-                    <p className="text-xxs">Everyone</p>
-                  </div>
-                </div>
-              </div>
+              <ConnectControl />
               <div className="flex flex-col space-y-2">
                 <div className="rounded-md px-3 py-4 w-[100%] h-[100%] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/10 shadow-md flex flex-row items-center space-x-2">
-                  <div className="h-7 w-7 bg-blue-600 rounded-full flex justify-center items-center">
+                  <div className="h-7 w-7 bg-[#167AE5] rounded-full flex justify-center items-center">
                     <IoMan style={{ color: 'white' }} />
                   </div>
                   <div>
