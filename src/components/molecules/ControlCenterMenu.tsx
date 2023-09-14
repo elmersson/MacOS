fcc53bdab102b93fda59850b63b977b021c4d155
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-14 07:14:32 +0200

    refactor: name switch

--- src/components/molecules/ControlCenterMenu.tsx
+++ src/components/molecules/ControlCenterMenu.tsx
@@ -3,7 +3,7 @@ import { IoPauseSharp, IoPlayForward, IoApps } from 'react-icons/io5';
 import Image from 'next/image';
 import Stockholmsvy from '../../assets/images/stockholmsvy.jpeg';
 import ConnectControl from './ConnectControl';
-import Darkmode from '../atoms/DarkMode';
+import ThemeSwitcher from '../atoms/ThemeSwitcher';
 
 interface ControlCenterMenuProps {
   isVisible: boolean;
@@ -40,7 +40,7 @@ export default function ControlCenterMenu({
             <div className="flex flex-row space-x-2">
               <ConnectControl />
               <div className="flex flex-col space-y-2">
-                <Darkmode />
+                <ThemeSwitcher />
                 <div className="flex flex-row space-x-2">
                   <div className="rounded-md px-3 py-1 w-[100%] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/10 shadow-md flex justify-center items-center flex-col">
                     <div className="flex justify-center items-center py-1">
