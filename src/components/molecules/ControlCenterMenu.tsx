9224adac34911049ff612d6b7aa29e91b29bf87e
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-18 15:54:21 +0200

    feat: stage and screen

--- src/components/molecules/ControlCenterMenu.tsx
+++ src/components/molecules/ControlCenterMenu.tsx
@@ -1,10 +1,10 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import { IoApps } from 'react-icons/io5';
 import ConnectControl from './ConnectControl';
 import ThemeSwitcher from '../atoms/ThemeSwitcher';
 import Sound from '../atoms/Sound';
 import Display from '../atoms/Display';
 import Music from '../atoms/Music';
+import StageNScreen from './StageNScreen';
 
 interface ControlCenterMenuProps {
   isVisible: boolean;
@@ -42,24 +42,7 @@ export default function ControlCenterMenu({
               <ConnectControl />
               <div className="flex flex-col space-y-2">
                 <ThemeSwitcher />
-                <div className="flex flex-row space-x-2">
-                  <div className="rounded-md px-3 py-1 w-[100%] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/10 shadow-md flex justify-center items-center flex-col">
-                    <div className="flex justify-center items-center py-1">
-                      <IoApps style={{ color: 'white', fontSize: '24px' }} />
-                    </div>
-                    <div>
-                      <p className="text-xxs text-center">Stage Manager</p>
-                    </div>
-                  </div>
-                  <div className="rounded-md px-3 py-1 w-[100%] bg-clip-padding backdrop-filter backdrop-blur-3xl bg-slate-200/10 shadow-md flex justify-center items-center flex-col">
-                    <div className="flex justify-center items-center py-1">
-                      <IoApps style={{ color: 'white', fontSize: '24px' }} />
-                    </div>
-                    <div>
-                      <p className="text-xxs text-center">Screen Mirroring</p>
-                    </div>
-                  </div>
-                </div>
+                <StageNScreen />
               </div>
             </div>
             <Display />
