33833a24ac2f40fe46ac22d841fa30216dfd3788
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-26 17:11:11 +0200

    feat: apple menu functionality

--- src/components/molecules/Apple.tsx
+++ src/components/molecules/Apple.tsx
@@ -1,3 +1,4 @@
+import { useStore } from '@/lib/store';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import { BsShift } from 'react-icons/bs';
@@ -10,6 +11,7 @@ import { LuCommand, LuOption, LuPower } from 'react-icons/lu';
 
 export default function Apple() {
   const [isVisible, setIsVisible] = useState<Boolean>(false);
+  const { setBooted, setLogedIn } = useStore();
 
   const variants = {
     hidden: {
@@ -26,8 +28,17 @@ export default function Apple() {
     },
   };
 
-  const handleClick = () => {
+  const handleVisibility = () => {
     setIsVisible(!isVisible);
+  };
+
+  const handleQuit = () => {
+    setBooted(false);
+    setLogedIn(false);
+  };
+
+  const handleSignOut = () => {
+    setLogedIn(false);
   };
   return (
     <div>
@@ -35,7 +46,7 @@ export default function Apple() {
         className={`px-2.5 rounded-md py-1 ${
           isVisible ? 'bg-slate-100/20' : ''
         }`}
-        onClick={handleClick}
+        onClick={handleVisibility}
       >
         <IoLogoApple style={{ color: 'white', fontSize: '18px' }} />
       </div>
@@ -48,12 +59,17 @@ export default function Apple() {
             variants={variants}
             className="fixed top-10 left-[4px] z-50 overflow-hidden shadow-lg"
           >
-            <div className="w-72 flex flex-col menubg space-y-2 p-3">
-              <p className="text-xs">About This Mac</p>
+            <div className="w-72 flex flex-col menubg">
+              <div className="menuItem">
+                <p className="text-xs">About This Mac</p>
+              </div>
               <div className="border-b border-gray-500/20"></div>
-              <p className="text-xs">System Settings...</p>
-              <div className="flex flex-row justify-between">
-                <div className="flex flex-row items-center space-x-2">
+              <div className="menuItem">
+                <p className="text-xs">System Settings...</p>
+              </div>
+
+              <div className="menuItem">
+                <div className="flex flex-row items-center">
                   <p className="text-xs">App Store...</p>
                 </div>
                 <div className="flex flex-row items-center px-3 rounded-xl bg-slate-400/50">
@@ -61,58 +77,47 @@ export default function Apple() {
                 </div>
               </div>
               <div className="border-b border-gray-500/20"></div>
-              <div className="flex flex-row justify-between items-center">
+              <div className="menuItem">
                 <div className="flex flex-row items-center space-x-2">
                   <p className="text-xs">Recent Items</p>
                 </div>
-                <IoChevronForwardOutline
-                  style={{ color: 'black', fontSize: '14px' }}
-                />
+                <IoChevronForwardOutline />
               </div>
               <div className="border-b border-gray-500/20" />
-              <div className="flex flex-row justify-between items-center">
+              <div className="menuItem" onClick={handleQuit}>
                 <p className="text-xs">Force Quit</p>
                 <div className="flex flex-row space-x-1">
-                  <LuOption
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <LuCommand
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <LuPower
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
+                  <LuOption />
+                  <LuCommand />
+                  <LuPower />
                 </div>
               </div>
               <div className="border-b border-gray-500/20"></div>
-              <p className="text-xs">Sleep</p>
-              <p className="text-xs">Restart...</p>
-              <p className="text-xs">Shut Down...</p>
+              <div className="menuItem" onClick={handleSignOut}>
+                <p className="text-xs">Sleep</p>
+              </div>
+              <div className="menuItem" onClick={handleQuit}>
+                <p className="text-xs">Restart...</p>
+              </div>
+              <div className="menuItem" onClick={handleQuit}>
+                <p className="text-xs">Shut Down...</p>
+              </div>
+
               <div className="border-b border-gray-500/20"></div>
-              <div className="flex flex-row justify-between items-center">
+              <div className="menuItem" onClick={handleSignOut}>
                 <p className="text-xs">Lock Screen</p>
                 <div className="flex flex-row space-x-1">
-                  <IoChevronUpOutline
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <LuCommand
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <LuPower
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
+                  <IoChevronUpOutline />
+                  <LuCommand />
+                  <LuPower />
                 </div>
               </div>
-              <div className="flex flex-row justify-between items-center">
+              <div className="menuItem" onClick={handleSignOut}>
                 <p className="text-xs">Log Out Rasmus Elmersson...</p>
                 <div className="flex flex-row space-x-1 items-center">
-                  <BsShift
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <LuCommand
-                    style={{ color: 'rgb(51 65 85 / 0.8)', fontSize: '12px' }}
-                  />
-                  <p className="text-xs text-slate-700/80">Q</p>
+                  <BsShift />
+                  <LuCommand />
+                  <span>Q</span>
                 </div>
               </div>
             </div>
