96003cd7fd0456b71ecafdcdbfc041becc0f10ff
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-28 08:03:44 +0200

    feat: new sonoma wallpaper

--- src/components/Pages/Login.tsx
+++ src/components/Pages/Login.tsx
@@ -1,17 +1,22 @@
 'use client';
 import Image from 'next/image';
 import Navbar from '../organisms/Navbar';
-import Cancel from '../../assets/icons/cancel-button-svgrepo-com.svg';
 import { useStore } from '@/lib/store';
 import { useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import AccountImage from '@/assets/images/Rasmus bakgrund.jpg';
+import useClock from '@/hooks/useClock';
+import getFullFormatDate from '@/lib/Date/getFullFormatDate';
 
 export default function Login() {
   const [inputValue, setInputValue] = useState('');
   const [isPasswordCorrect, setIsPasswordCorrect] = useState(true);
+  const [showInput, setShowInput] = useState(false);
 
-  const { setLogedIn, setBooted } = useStore();
+  const currentTime = useClock();
+  const timeObject = getFullFormatDate(currentTime);
+
+  const { setLogedIn } = useStore();
   const inputAnimationControls = useAnimation();
 
   const password = 'RasmusRocks!!!';
@@ -36,55 +41,62 @@ export default function Login() {
     }
   };
 
-  const handleClick = () => {
-    setBooted(false);
-  };
-
   return (
-    <div>
+    <div
+      className="min-h-screen flex flex-col"
+      onClick={() => setShowInput(true)}
+    >
       <Navbar />
-      <div className="flex flex-col items-center mt-80">
-        <div className="w-48 h-48 bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30 rounded-full p-2">
-          <Image
-            src={AccountImage}
-            alt="account image"
-            className="rounded-full drop-shadow"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
+      <div className="flex flex-col h-screen justify-between">
+        <div className="flex flex-col items-center mt-10">
+          <text className="text-2xl font-bold text-white">
+            {timeObject.dayOfWeek}, {timeObject.dayOfMonth} {timeObject.month}
+          </text>
+          <text className="text-[6.5rem] font-black text-white">
+            {timeObject.time}
+          </text>
         </div>
-        <p className="text-white font-semibold text-2xl text-shadow-lg mt-12">
-          Rasmus Elmersson
-        </p>
-        <motion.div
-          className={`w-44 h-8 px-4 rounded-2xl bg-sky-200/30 backdrop-blur-sm mt-4 drop-shadow p-1 ${
-            isPasswordCorrect ? '' : 'shake'
-          }`}
-          animate={inputAnimationControls}
-        >
-          <input
-            placeholder="Enter Password"
-            onBlur={handlePasswordValidation}
-            onChange={handleInputChange}
-            className=" focus:none outline-none text-white bg-transparent placeholder-sky-50/80 text-shadow"
-            onKeyDown={handleKeyPress}
-          />
-        </motion.div>
-        {!isPasswordCorrect && (
-          <p className="text-white/80 text-shadow-lg mt-2 text-xs">
-            Test {password}
-          </p>
-        )}
-        <p className="text-white text-shadow-lg mt-2">Enter Password</p>
-        <div className="flex flex-col items-center mt-80">
-          <div
-            className="w-9 h-9 bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30 rounded-full p-2"
-            onClick={handleClick}
-          >
-            <Image src={Cancel} alt="cancel" />
+        <div className="flex flex-col items-center mb-36">
+          <div className="w-20 h-20 bg-slate-700 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30 rounded-full p-2">
+            <Image
+              src={AccountImage}
+              alt="account image"
+              className="rounded-full drop-shadow"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
           </div>
-          <p className="text-white text-shadow text-sm mt-2">Cancel</p>
+          {!showInput ? (
+            <p className="text-white font-semibold text-sm text-shadow-lg mt-2">
+              Rasmus Elmersson
+            </p>
+          ) : (
+            <motion.div
+              className={`flex items-center w-32 h-6 px-2 rounded-2xl bg-sky-200/30 backdrop-blur-sm mt-2 drop-shadow p-1 ${
+                isPasswordCorrect ? '' : 'shake'
+              }`}
+              animate={inputAnimationControls}
+            >
+              <input
+                placeholder="Enter Password"
+                onBlur={handlePasswordValidation}
+                onChange={handleInputChange}
+                className="focus:none outline-none text-white bg-transparent placeholder-sky-50/80 text-shadow text-xs"
+                onKeyDown={handleKeyPress}
+              />
+            </motion.div>
+          )}
+
+          {isPasswordCorrect ? (
+            <p className="text-white text-shadow-lg mt-1 text-xs">
+              Enter Password
+            </p>
+          ) : (
+            <p className="text-white/80 text-shadow-lg mt-1 text-xs">
+              Test {password}
+            </p>
+          )}
         </div>
       </div>
     </div>
