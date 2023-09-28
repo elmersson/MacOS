71a70ee60a43f70a1c5dc52424cc1ec5cb524bb1
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-09-28 08:03:44 +0200

    feat: new sonoma wallpaper

--- src/components/atoms/Wallpapers.tsx
+++ src/components/atoms/Wallpapers.tsx
@@ -1,20 +1,41 @@
-import Image from 'next/image';
-import WallpaperDay from '../../assets/images/wallpaper-day.jpg';
-import WallpaperNight from '../../assets/images/wallpaper-night.jpg';
-import { useTheme } from 'next-themes';
+import { useStore } from '@/lib/store';
+import { useEffect, useRef } from 'react';
 
 export default function Wallpapers() {
-  const { theme } = useTheme();
+  const { logedIn, booted } = useStore();
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+      if (booted && !logedIn) {
+        video.play();
+      } else {
+        video.playbackRate = 0.5;
+
+        setTimeout(() => {
+          video.pause();
+          video.playbackRate = 1;
+        }, 2000);
+      }
+    }
+  }, [booted, logedIn]);
 
   return (
     <div className={`absolute inset-0 -z-50 bg-black`}>
-      <Image
-        src={theme === 'light' ? WallpaperDay : WallpaperNight}
-        alt="background image"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-      />
+      <video
+        ref={videoRef}
+        src="https://sylvan.apple.com/Videos/P001_C005_UHD_SDR_2K_AVC.mov"
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        }}
+        autoPlay
+        loop
+        playsInline
+        muted
+      ></video>
     </div>
   );
 }
