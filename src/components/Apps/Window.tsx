3206c20bd38b428004abc5d98f7bca5d14e18c68
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-10-17 19:51:22 +0200

    feat: rnd base

--- src/components/Apps/Window.tsx
+++ src/components/Apps/Window.tsx
@@ -1,31 +1,68 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { RxCross2 } from 'react-icons/rx';
 import { PiMinusBold } from 'react-icons/pi';
 import { RiExpandLeftRightFill } from 'react-icons/ri';
+import useScreenSize from '@/hooks/useScreenSize';
 
+interface WindowState {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}
 export default function Window() {
+  const { scrHeight, scrWidth } = useScreenSize();
   const [hovered, setHovered] = useState(false);
-  const [position, setPosition] = useState<{ x: number; y: number }>({
-    x: 100,
-    y: 100,
+
+  const initWidth = Math.min(scrWidth || 640);
+  const initHeight = Math.min(scrHeight || 400);
+
+  const [state, setState] = useState<WindowState>({
+    width: initWidth,
+    height: initHeight,
+    x: scrWidth + (scrWidth - initWidth) / 2,
+    y: (scrHeight - initHeight - 11) * 2,
   });
 
-  const handleDragStop = (_e: any, d: { x: number; y: number }) => {
-    setPosition({
-      x: d.x,
-      y: d.y,
+  useEffect(() => {
+    setState({
+      ...state,
+      width: Math.min(scrWidth, state.width),
+      height: Math.min(scrHeight, state.height),
     });
+  }, [scrWidth, scrHeight, state]);
+
+  const handleDoubleClick = () => {
+    setState({ width: scrWidth, height: scrHeight * 2, x: 0, y: 11 });
   };
 
   return (
     <Rnd
       bounds="parent"
-      size={{ width: 800, height: 800 }}
-      position={{ x: position.x, y: position.y }}
-      onDragStop={handleDragStop}
+      size={{ width: state.width, height: state.height }}
+      position={{
+        x: Math.max(0, Math.min(scrWidth * 2 - state.width, state.x)),
+        y: Math.min(scrHeight - 11, Math.max(0, state.y)),
+      }}
+      onDragStop={(e, d) => {
+        setState({ ...state, x: d.x, y: d.y });
+      }}
+      onResizeStop={(e, direction, ref, delta, position) => {
+        setState({
+          ...state,
+          width: parseInt(ref.style.width),
+          height: parseInt(ref.style.height),
+          ...position,
+        });
+      }}
+      minWidth={200}
+      minHeight={150}
     >
-      <div className="items-center justify-center bg-slate-50/90 rounded-t-md py-1 flex flex-row">
+      <div
+        className="items-center bg-[#3c3c3c] rounded-t-md py-1 flex flex-row round"
+        onDoubleClick={handleDoubleClick}
+      >
         <div
           className="flex items-center"
           onMouseEnter={() => setHovered(true)}
@@ -57,9 +94,14 @@ export default function Window() {
             )}
           </div>
         </div>
-        <p className="flex-1 text-center">Window</p>
       </div>
-      <div className="w-full h-[500px] bg-black"></div>
+      <div className="w-full h-[500px] bg-black">
+        <iframe
+          className="w-full h-full bg-black"
+          title="github"
+          src="https://github1s.com/elmersson/MacOS"
+        />
+      </div>
     </Rnd>
   );
 }
