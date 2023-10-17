bddc79592473be4809c4861cae1450ffe1e4aaee
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-10-17 19:51:22 +0200

    feat: rnd base

--- src/components/Pages/Desktop.tsx
+++ src/components/Pages/Desktop.tsx
@@ -1,18 +1,10 @@
-import useScreenSize from '@/hooks/useScreenSize';
 import Window from '../Apps/Window';
 import Dock from '../Dock/Dock';
 import Navbar from '../Navbar/Navbar';
 
 export default function Desktop() {
-  const state = useScreenSize();
-
-  console.log(state.scrHeight, state.scrWidth);
-
   return (
-    <div
-      className="flex flex-col overflow-hidden"
-      style={{ height: `${state.scrHeight}px`, width: `${state.scrWidth}px` }}
-    >
+    <div className="flex flex-col overflow-hidden">
       <Navbar />
       <Window />
       <Dock />
