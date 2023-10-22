19cf0e0f67c496821a846927495e7e392b2ff9b6
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-10-22 15:20:22 +0200

    test: add tests

--- __e2e/playwright.spec.ts
+++ __e2e/playwright.spec.ts
@@ -7,3 +7,17 @@ test.beforeEach(async ({ page }) => {
    test('has title', async ({ page }) => {  
     await expect(page).toHaveTitle(/Rasmus Elmersson/);
   });
+
+  test('has boot', async ({ page }) => {
+    const progressBar = await page.$('.h-1');
+    expect(progressBar).not.toBeNull();
+
+    await page.waitForFunction(() => {
+      const progress = document.querySelector('.h-1') as HTMLElement;
+      return progress.style.width === '100%';
+    });
+  });
+
+  test('has booted', async ({ page }) => {
+    await expect(page.getByText('Rasmus Elmersson')).toBeDefined();
+  });
--- src/components/Pages/Boot.tsx
+++ src/components/Pages/Boot.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
-import Image from 'next/image';
-import Apple from '@/assets/icons/apple.svg';
 import { useState, useEffect } from 'react';
 import { useStore } from '@/lib/store';
 import useClock from '@/hooks/useClock';
 import getFullFormatDate from '@/lib/Date/getFullFormatDate';
 import axios from 'axios';
 import { WeatherData } from '@/data/Weather/WeatherData';
+import { IoLogoApple } from 'react-icons/io5';
 
 export default function Boot() {
   const [progress, setProgress] = useState<number>(0);
@@ -95,7 +94,12 @@ export default function Boot() {
 
   return (
     <div className="min-h-screen w-full flex flex-col items-center bg-black justify-center">
-      <Image src={Apple} alt="Apple logo" width={130} />
+      <IoLogoApple
+        style={{
+          color: 'white',
+          fontSize: '130px',
+        }}
+      />
       <div className="h-1.5 w-64 rounded-full bg-neutral-700/80 border border-neutral-100/20 mt-[5%]">
         <div
           className="h-1 rounded-full bg-white border"
