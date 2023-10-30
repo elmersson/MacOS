33d18fb88fb1b4927637df6cbb877ffc38044f68
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-10-30 19:33:22 +0100

    style: dockitem title

--- src/components/Dock/DockItem.tsx
+++ src/components/Dock/DockItem.tsx
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { AppData } from '@/data/Apps';
-import { motion } from 'framer-motion';
 
 export default function DockItem({ id, title, img }: AppData) {
-  const [isHovered, setIsHovered] = useState(false);
   const [isActive, setIsActive] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
 
   const handleClick = () => {
     setIsActive(!isActive);
@@ -21,24 +11,19 @@ export default function DockItem({ id, title, img }: AppData) {
 
   return (
     <li
-      className="flex justify-center"
+      className="flex justify-center relative group"
       id={id}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
       onClick={handleClick}
     >
-      <div className="flex flex-col items-center relative">
-        <motion.div
-          className={`absolute bg-clip-padding backdrop-filter backdrop-blur-md top-[-60px] bg-neutral-300 text-sm ${
-            isHovered ? 'opacity-100' : 'opacity-0'
-          } transition-all duration-100 py-1 px-2 rounded-md`}
-        >
+      <div className="flex flex-col items-center">
+        <div className="absolute bg-clip-padding backdrop-filter backdrop-blur-md top-[-60px] bg-neutral-300 text-sm opacity-0 transition-all duration-100 py-1 px-2 rounded-md group-hover:opacity-100">
           <p className="text-black whitespace-nowrap overflow-hidden overflow-ellipsis">
             {title}
           </p>
-        </motion.div>
+          <div className="before:absolute before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-0 before:border-solid before:border-transparent before:border-t-4 before:border-l-4 before:border-r-4 before:border-b-0 before:border-neutral-300 dark:before:border-white before:transform before:-rotate-180 before:content-[' ']"></div>
+        </div>
         <Image src={img} alt={title} className="w-14" />
-        <motion.div
+        <div
           className={`h-1 w-1 m-0 rounded-full bg-slate-950/80 dark:bg-slate-50/80 mt-1 ${
             isActive ? 'opacity-100' : 'opacity-0'
           } transition-all duration-100`}
